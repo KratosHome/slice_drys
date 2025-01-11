de0c1f39d1e5e6ee8deea1af1e17e86fd1fd53a3
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-11 15:26:04 +0200

    feat(blog) list

--- src/components/client/blog/blog.tsx
+++ src/components/client/blog/blog.tsx
@@ -1,5 +1,6 @@
+// Імпорт компонентів і хуків
 'use client'
-import { useState, useEffect } from 'react' // Імпорт хуків
+import { useState, useEffect } from 'react'
 import { getPosts } from '@/server/posts/get-posts.server'
 import {
   Pagination,
@@ -15,56 +16,104 @@ interface BlogProps {
   locale: string
 }
 
+// Компонент блогу
 export default function Blog({ locale }: BlogProps) {
-  const [page, setPage] = useState(1)
-  const [posts, setPosts] = useState<IPost[]>([])
+  const [page, setPage] = useState(1) // Стан для зберігання поточної сторінки
+  const [posts, setPosts] = useState<IPost[]>([]) // Стан для зберігання дописів
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const data = await getPosts(locale, page, 8)
+      const data = await getPosts(locale, page, 8) // Отримання дописів
       const posts = data.post
       setPosts(posts)
     }
 
-    fetchPosts()
-  }, [locale, page])
+    fetchPosts() // Виклик функції для отримання дописів
+  }, [locale, page]) // Залежність від locale і page
 
   const handleNext = () => {
-    setPage((prevPage) => prevPage + 1)
+    setPage((prevPage) => prevPage + 1) // Збільшення номера сторінки
   }
 
   const handlePrevious = () => {
     if (page > 1) {
-      setPage((prevPage) => prevPage - 1)
+      setPage((prevPage) => prevPage - 1) // Зменшення номера сторінки
     }
   }
 
   return (
     <div>
-      {posts.length > 0 && (
+      {posts.length > 0 && ( // Перевірка наявності дописів
         <>
-          <div>{JSON.stringify(posts[3].img)}</div>
-          <div>{JSON.stringify(posts[1].img)}</div>
-
-          <Image
-            width={250}
-            height={250}
-            src={posts[2].img}
-            alt="1"
-            // className="h-10 w-10 rounded object-cover"
-          />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {posts.slice(0, 2).map(
+              (
+                post,
+                index, // Ітерація по перших двох дописах
+              ) => (
+                <div key={index}>
+                  <h2>{post.title}</h2> {/* Відображення заголовку допису */}
+                  <Image
+                    width={250}
+                    height={250}
+                    src={post.img}
+                    alt={`Image ${index}`} // Альтернативний текст з номером зображення
+                  />
+                </div>
+              ),
+            )}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {posts.slice(2, 4).map(
+              (
+                post,
+                index, // Ітерація по решті дописів
+              ) => (
+                <div key={index}>
+                  <h2>{post.title}</h2> {/* Відображення заголовку допису */}
+                  <Image
+                    width={250}
+                    height={250}
+                    src={post.img}
+                    alt={`Image ${index + 2}`} // Альтернативний текст з номером зображення
+                  />
+                </div>
+              ),
+            )}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            {posts.slice(4).map(
+              (
+                post,
+                index, // Ітерація по решті дописів
+              ) => (
+                <div key={index}>
+                  <h2>{post.title}</h2> {/* Відображення заголовку допису */}
+                  <Image
+                    width={250}
+                    height={250}
+                    src={post.img}
+                    alt={`Image ${index + 2}`} // Альтернативний текст з номером зображення
+                  />
+                </div>
+              ),
+            )}
+          </div>
         </>
       )}
       <Pagination>
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious href="#" onClick={handlePrevious} />
+            <PaginationPrevious href="#" onClick={handlePrevious} />{' '}
+            {/* Кнопка попередня сторінка */}
           </PaginationItem>
           <PaginationItem>
-            <PaginationLink href="#">{page}</PaginationLink>
+            <PaginationLink href="#">{page}</PaginationLink>{' '}
+            {/* Поточна сторінка */}
           </PaginationItem>
           <PaginationItem>
-            <PaginationNext href="#" onClick={handleNext} />
+            <PaginationNext href="#" onClick={handleNext} />{' '}
+            {/* Кнопка наступна сторінка */}
           </PaginationItem>
         </PaginationContent>
       </Pagination>
