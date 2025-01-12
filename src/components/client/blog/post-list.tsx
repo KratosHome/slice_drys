7e17834c7bb009d988369845897fae472a1c8155
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-12 15:09:25 +0200

    fix (blog) styles

--- src/components/client/blog/post-list.tsx
+++ src/components/client/blog/post-list.tsx
@@ -26,27 +26,27 @@ export default function PostList({ posts }: BlogProps) {
 
           <div className="mb-10 mt-10 flex justify-between">
             <div className="flex w-full flex-wrap">
-              <div className="flex w-1/2 sm:w-1/3">
+              <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[2]} />
                 <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-              <div className="flex w-1/2 sm:w-1/3">
+              <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[3]} />
                 <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-              <div className="flex w-1/2 sm:w-1/3">
+              <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[4]} />
                 <div className="m-1 ml-2 h-full border-l border-dashed border-white"></div>
               </div>
-              <div className="flex w-1/2 sm:w-1/3">
+              <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[5]} />
                 <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-              <div className="flex w-1/2 sm:w-1/3">
+              <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[6]} />
                 <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-              <div className="flex w-1/2 sm:w-1/3">
+              <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[7]} />
                 <div className="m-1 ml-2 h-full border-l border-dashed border-white"></div>
               </div>
