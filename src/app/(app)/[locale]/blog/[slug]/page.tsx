68281aec50db717c43416fffd12602c88d1f1558
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-12 15:31:29 +0200

    fix (blog) local

--- src/app/(app)/[locale]/blog/[slug]/page.tsx
+++ src/app/(app)/[locale]/blog/[slug]/page.tsx
@@ -1,19 +1,18 @@
-// Імпорти
 import { getPostBySlug } from '@/server/posts/get-posts.server'
-import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html' // Імпортуємо бібліотеку
+import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html'
 
 export default async function PostPage({
   params,
 }: {
-  params: { slug: string }
+  params: { slug: string; locale: string }
 }) {
-  const slug = params.slug
+  const { slug, locale } = params // Деструктуруємо slug і locale з params
 
-  const data: IGetOnePost = await getPostBySlug('uk', slug)
+  const data: IGetOnePost = await getPostBySlug(locale, slug)
 
-  const content = data.post.content
+  const content = JSON.parse(data.post.content) // Перетворюємо із JSON-рядка в об'єкт
 
-  const converter = new QuillDeltaToHtmlConverter(content.ops)
+  const converter = new QuillDeltaToHtmlConverter(content.ops) // Передаємо Delta об'єкт
   const html = converter.convert()
 
   return (
