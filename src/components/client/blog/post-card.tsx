23c8b9b694d0493f64687c32812051fb35ef9e1f
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-12 15:04:51 +0200

    fix (blog) styles

--- src/components/client/blog/post-card.tsx
+++ src/components/client/blog/post-card.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useLocale } from 'next-intl'
+import { Post } from '@/server/posts/postSchema'
 
 interface PostCardProps {
-  key: any
-  image: string
-  title: string
-  date: string
+  post?: IPost
+  key?: any
+  image?: string
+  title?: string
+  date?: string
   slag?: string
 }
 
 export default function PostCard({
+  post,
   key,
   image,
   title,
@@ -18,21 +21,29 @@ export default function PostCard({
   slag,
 }: PostCardProps) {
   const local = useLocale()
-  slag = slag ? slag : '111'
+
+  if (!post) {
+    return null
+  }
 
   return (
-    <Link href={`/${local}/blog/${slag}`} className="w-full">
-      <div key={key} className="ml-5 mr-5 flex-1 cursor-pointer">
+    <Link href={`/${local}/blog/${post?.slug}`} className="w-full">
+      <div className="ml-5 mr-5 flex-1 cursor-pointer">
         <div className="relative w-full pb-[75%]">
-          <Image src={image} alt="img" layout="fill" objectFit="cover" />
+          <Image
+            src={post?.img || ''}
+            alt="img"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
         <div>
-          <h3 className="text-left font-poppins text-lg font-normal text-gray-400">
-            {date}
-          </h3>
-          <h2 className="text-left font-poppins text-2xl font-semibold">
-            {title}
-          </h2>
+          <p className="text-left font-poppins text-lg font-normal text-gray-400">
+            {new Date(post.updatedAt).toLocaleDateString('uk-UA')}
+          </p>
+          <p className="line-clamp-2 break-all text-left font-poppins text-2xl font-semibold">
+            {post?.title}
+          </p>
         </div>
       </div>
     </Link>
--- src/components/client/blog/post-list.tsx
+++ src/components/client/blog/post-list.tsx
@@ -7,65 +7,50 @@ interface BlogProps {
 export default function PostList({ posts }: BlogProps) {
   return (
     <div>
-      {' '}
       {posts.length > 0 && (
         <div className="m-20">
           <div className="mb-10 mt-10 flex justify-between">
-            {posts.slice(0, 2).map((post, index) => (
-              <div className="flex w-full" key={index}>
-                <div key={index} className="flex w-full">
-                  <PostCard
-                    key={index}
-                    image={post.img}
-                    date={new Date(post.updatedAt).toLocaleDateString('uk-UA')}
-                    title={post.title}
-                    slag={post.slug}
-                  />
-                </div>
-                {index !== 1 && (
-                  <div className="m-1 ml-2 h-full border-l border-dashed border-black"></div>
-                )}
+            <div className="flex w-full flex-wrap">
+              <div className="flex sm:w-1/2">
+                <PostCard post={posts[0]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-            ))}
+              <div className="flex sm:w-1/2">
+                <PostCard post={posts[1]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white"></div>
+              </div>
+            </div>
           </div>
+
           <div className="my-4 border-t border-dashed border-black"></div>
 
           <div className="mb-10 mt-10 flex justify-between">
-            {posts.slice(2, 5).map((post, index) => (
-              <div className="flex w-full" key={index}>
-                <div key={index} className="flex w-full items-stretch">
-                  <PostCard
-                    key={index}
-                    image={post.img}
-                    date={new Date(post.updatedAt).toLocaleDateString('uk-UA')}
-                    title={post.title}
-                    slag={post.slug}
-                  />
-                </div>
-                {index !== 2 && (
-                  <div className="m-1 ml-2 h-full border-l border-dashed border-black"></div>
-                )}
+            <div className="flex w-full flex-wrap">
+              <div className="flex w-1/2 sm:w-1/3">
+                <PostCard post={posts[2]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-            ))}
-          </div>
-
-          <div className="mb-10 mt-10 flex justify-between">
-            {posts.slice(5).map((post, index) => (
-              <div className="flex w-full" key={index}>
-                <div key={index} className="flex w-full items-stretch">
-                  <PostCard
-                    key={index}
-                    image={post.img}
-                    date={new Date(post.updatedAt).toLocaleDateString('uk-UA')}
-                    title={post.title}
-                    slag={post.slug}
-                  />
-                </div>
-                {index !== 2 && (
-                  <div className="m-1 ml-2 h-full border-l border-dashed border-black"></div>
-                )}
+              <div className="flex w-1/2 sm:w-1/3">
+                <PostCard post={posts[3]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
-            ))}
+              <div className="flex w-1/2 sm:w-1/3">
+                <PostCard post={posts[4]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white"></div>
+              </div>
+              <div className="flex w-1/2 sm:w-1/3">
+                <PostCard post={posts[5]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
+              </div>
+              <div className="flex w-1/2 sm:w-1/3">
+                <PostCard post={posts[6]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
+              </div>
+              <div className="flex w-1/2 sm:w-1/3">
+                <PostCard post={posts[7]} />
+                <div className="m-1 ml-2 h-full border-l border-dashed border-white"></div>
+              </div>
+            </div>
           </div>
         </div>
       )}
