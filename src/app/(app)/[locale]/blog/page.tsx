d098b22cbafd501ed91680804b9869d81b804839
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-13 14:19:21 +0200

    feat (blog) Breadcrumb

--- src/app/(app)/[locale]/blog/page.tsx
+++ src/app/(app)/[locale]/blog/page.tsx
@@ -8,33 +8,81 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/client/ui/pagination'
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/client/ui/breadcrumbs'
+import { useTranslations } from 'next-intl'
 
 type Props = {
   params: { locale: string }
   searchParams: { page?: string }
 }
 
-export default async function Home({ params, searchParams }: Props) {
+// Основна асинхронна функція для отримання даних блогу
+export default async function Blog({ params, searchParams }: Props) {
   const { locale } = params
   const page = parseInt(searchParams.page || '1', 10)
-  const data = await getPosts(locale, page, 8)
+  const data = await getPosts(locale, page, 8) // Отримати пости асинхронно
+
+  return <RenderContent locale={locale} page={page} data={data} /> // Виклик компонента
+}
+
+// Компонент React, що відображає контент
+function RenderContent({
+  locale,
+  page,
+  data,
+}: {
+  locale: string
+  page: number
+  data: any
+}) {
+  const t = useTranslations('Breadcrumbs')
 
   return (
-    <main className="mx-auto flex max-w-[1280px] flex-col items-center">
-      <PostList posts={data.post} />
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href={`?page=${page > 1 ? page - 1 : 1}`} />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href={`?page=${page}`}>{page}</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href={`?page=${page + 1}`} />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+    <main className="max-w-[1280px]">
+      <div className="mt-10">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">{t('Home')}</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href={`/${locale}/blog`}>
+                {t('Blog')}
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage> {page}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+
+      <div className="mx-auto flex flex-col items-center">
+        <PostList posts={data.post} />
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href={`?page=${page > 1 ? page - 1 : 1}`} />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href={`?page=${page}`}>{page}</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href={`?page=${page + 1}`} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </main>
   )
 }
