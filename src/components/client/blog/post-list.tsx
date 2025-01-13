f7bccd24dd5c3fad0ffe4cd972c53afc2ec7077b
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-13 08:50:23 +0200

    fix (blog) styles

--- src/components/client/blog/post-list.tsx
+++ src/components/client/blog/post-list.tsx
@@ -11,12 +11,12 @@ export default function PostList({ posts }: BlogProps) {
         <div>
           <div className="mb-10 mt-10 flex justify-between">
             <div className="flex w-full flex-wrap justify-between">
-              <div className="flex sm:w-[45%]">
+              <div className="flex w-[100%] sm:w-[45%]">
                 <PostCard post={posts[0]} />
               </div>
-              <div className="m-1 ml-2 hidden h-full border-l border-dashed border-black pr-1 sm:block sm:w-[1%]"></div>
+              <div className="ml-1 hidden h-full border-l border-dashed border-black sm:block sm:w-[1%]"></div>
 
-              <div className="flex sm:w-[45%]">
+              <div className="flex w-[100%] sm:w-[45%]">
                 <PostCard post={posts[1]} />
               </div>
             </div>
@@ -26,7 +26,6 @@ export default function PostList({ posts }: BlogProps) {
             <div className="flex w-full flex-wrap">
               <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[2]} />
-                <div className="m-1 ml-2 h-full border-l border-dashed border-white sm:border-black"></div>
               </div>
               <div className="mb-10 flex w-1/2 sm:w-1/3">
                 <PostCard post={posts[3]} />
