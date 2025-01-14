efbbab1998a72b6c29854626d0a88e637410172b
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-14 21:24:23 +0200

    fix (blog) post card animation depth

--- src/components/client/blog/post-card.tsx
+++ src/components/client/blog/post-card.tsx
@@ -5,51 +5,45 @@ import { CardContainer, CardBody, CardItem } from '@/components/ui/3d-card'
 
 interface PostCardProps {
   post?: IPost
-  key?: any
-  image?: string
-  title?: string
-  date?: string
-  slag?: string
 }
 
-export default function PostCard({
-  post,
-  key,
-  image,
-  title,
-  date,
-  slag,
-}: PostCardProps) {
+export default function PostCard({ post }: PostCardProps) {
   const local = useLocale()
 
   if (!post) {
     return null
   }
+  const rotateZ = Math.floor(Math.random() * 20) - 10
 
   return (
     <CardContainer className="w-full">
       <CardBody>
         <CardItem
-          as={Link}
+          translateZ={-100}
+          rotateZ={rotateZ}
           href={`/${local}/blog/${post?.slug}`}
           className="flex-1 cursor-pointer"
         >
           <div className="relative w-full pb-[75%]">
-            <Image
-              src={post?.img || ''}
-              alt={post?.title}
-              layout="fill"
-              objectFit="cover"
-            />
+            <Link href={`/${local}/blog/${post?.slug}`}>
+              <Image
+                src={post?.img || ''}
+                alt={post?.title}
+                layout="fill"
+                objectFit="cover"
+              />
+            </Link>
           </div>
-          <div>
-            <p className="text-left font-poppins text-lg font-normal text-gray-400">
-              {new Date(post.updatedAt).toLocaleDateString('uk-UA')}
-            </p>
-            <p className="line-clamp-2 break-all text-left font-poppins text-2xl font-semibold">
-              {post?.title}
-            </p>
-          </div>
+        </CardItem>
+        <CardItem translateZ={20}>
+          <p className="text-left font-poppins text-lg font-normal text-gray-400">
+            {new Date(post.updatedAt).toLocaleDateString('uk-UA')}
+          </p>
+        </CardItem>
+        <CardItem rotateX={35} translateZ={100}>
+          <p className="line-clamp-2 break-all text-left font-poppins text-2xl font-semibold">
+            {post?.title}
+          </p>
         </CardItem>
       </CardBody>
     </CardContainer>
