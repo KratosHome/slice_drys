5d6b258ac96364dab70b3f7628f1c4e30403c4af
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-15 10:14:11 +0200

    feat (blog) social icons

--- src/app/(app)/[locale]/(client)/blog/[slug]/page.tsx
+++ src/app/(app)/[locale]/(client)/blog/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getPosts } from '@/server/posts/get-posts.server'
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html'
+import Link from 'next/link'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import {
   Breadcrumb,
@@ -23,12 +25,13 @@ export default async function PostPage({
   }
   const content = JSON.parse(data.post[0].content)
   const title = data.post[0].title
-
+  const date = new Date(data.post[0].updatedAt).toLocaleDateString('uk-UA')
+  const author = data.post[0].author
   const converter = new QuillDeltaToHtmlConverter(content.ops)
   const html = converter.convert()
 
   return (
-    <main className="mx-auto max-w-[1280px]">
+    <main className="mx-auto flex max-w-[1280px] flex-col justify-center">
       <div className="mt-10">
         <Breadcrumb>
           <BreadcrumbList>
@@ -57,6 +60,53 @@ export default async function PostPage({
       <div className="h-10"></div>
       <div className="mx-auto flex max-w-[1280px] flex-col items-center">
         <h1 dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+      <div className="my-20">
+        <div className="my-4 border-t border-dashed border-black"></div>
+        <div className="flex justify-between">
+          <div className="text-gray-500">{date}</div>
+          <div className="text-gray-500">{author}</div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-5 sm:flex-row">
+        <div
+          className="text-[26px]"
+          style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
+        >
+          Поділитись
+        </div>
+        <div className="flex gap-3">
+          <Link href="#" className="w-fit">
+            <Image
+              src={'/icons/facebook.svg'}
+              alt="facebook icon"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+            <div className="absolute bottom-0 left-0 right-0 top-0 group-data-[focus]:bg-red group-data-[focus]:blur-2xl"></div>
+          </Link>
+          <Link href="#" className="w-fit">
+            <Image
+              src={'/icons/instagram.svg'}
+              alt="facebook icon"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+            <div className="absolute bottom-0 left-0 right-0 top-0 group-data-[focus]:bg-red group-data-[focus]:blur-2xl"></div>
+          </Link>
+          <Link href="#" className="w-fit">
+            <Image
+              src={'/icons/telegram.svg'}
+              alt="facebook icon"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+            <div className="absolute bottom-0 left-0 right-0 top-0 group-data-[focus]:bg-red group-data-[focus]:blur-2xl"></div>
+          </Link>
+        </div>
       </div>
     </main>
   )
