b91f3c284721bbef77245ac2f11a5a307827ded8
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-16 09:42:33 +0200

    fix (blog) move select from common ui

--- src/components/client/product/product.tsx
+++ src/components/client/product/product.tsx
@@ -7,7 +7,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '@/components/ui/select'
+} from '@/components/client/ui/select'
 import Link from 'next/link'
 import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card'
 import Image from 'next/image'
