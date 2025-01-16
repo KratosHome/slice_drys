f562637abb427f8cb5be4e88fe97ce02b692a6e7
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-16 14:42:01 +0200

    fix (blog) safari image click

--- src/components/client/blog/post-card.tsx
+++ src/components/client/blog/post-card.tsx
@@ -20,36 +20,36 @@ export default function PostCard({ post }: PostCardProps) {
   const rotateZ = Math.floor(Math.random() * 20) - 10
 
   return (
-    <CardContainer className="w-full">
-      <CardBody>
-        <CardItem
-          translateZ={-100}
-          rotateZ={rotateZ}
-          href={`/${local}/blog/${post?.slug}`}
-          className="flex-1 cursor-pointer"
-        >
-          <div className="relative w-full pb-[75%]">
-            <Link href={`/${local}/blog/${post?.slug}`}>
+    <Link href={`/${local}/blog/${post?.slug}`}>
+      <CardContainer className="w-full">
+        <CardBody>
+          <CardItem
+            translateZ={-100}
+            rotateZ={rotateZ}
+            href={`/${local}/blog/${post?.slug}`}
+            className="flex-1 cursor-pointer"
+          >
+            <div className="relative w-full pb-[75%]">
               <Image
                 src={post?.img || ''}
                 alt={post?.title}
                 layout="fill"
                 objectFit="cover"
               />
-            </Link>
-          </div>
-        </CardItem>
-        <CardItem translateZ={20}>
-          <p className="text-left font-poppins text-lg font-normal text-gray-400">
-            {new Date(post.updatedAt).toLocaleDateString('uk-UA')}
-          </p>
-        </CardItem>
-        <CardItem rotateX={35} translateZ={100}>
-          <p className="line-clamp-2 break-all text-left font-poppins text-2xl font-semibold">
-            {post?.title}
-          </p>
-        </CardItem>
-      </CardBody>
-    </CardContainer>
+            </div>
+          </CardItem>
+          <CardItem translateZ={20}>
+            <p className="text-left font-poppins text-lg font-normal text-gray-400">
+              {new Date(post.updatedAt).toLocaleDateString('uk-UA')}
+            </p>
+          </CardItem>
+          <CardItem rotateX={35} translateZ={100}>
+            <p className="line-clamp-2 break-all text-left font-poppins text-2xl font-semibold">
+              {post?.title}
+            </p>
+          </CardItem>
+        </CardBody>
+      </CardContainer>
+    </Link>
   )
 }
