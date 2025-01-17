5825068a60e04ef89814f5f74a3c0f59f97d7631
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-17 16:30:33 +0200

    feat (about) add about +

--- src/components/client/about-us/about-us.tsx
+++ src/components/client/about-us/about-us.tsx
@@ -1,3 +1,44 @@
+import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+
 export default function AboutUs() {
-  return <div>1111111111</div>
+  const t = useTranslations('AboutUs')
+
+  return (
+    <div className="flex w-full flex-col items-center">
+      <div className="flex items-center">
+        <Image
+          src={'/icons/arc-arrow-down.svg'}
+          alt={t('facebook icon')}
+          width={42}
+          height={60}
+          className="mr-10 pt-28"
+        />
+        <div
+          className="w-[100%] text-[96px]"
+          style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
+        >
+          {t('about us')}
+        </div>
+      </div>
+      <div className="flex">
+        <div className="justify-center bg-black px-6 py-12">
+          <Image
+            src={'/icons/logo-white.svg'}
+            alt={t('facebook icon')}
+            width={120}
+            height={140}
+          />
+        </div>
+        <div
+          className="flex text-[36px]"
+          style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
+        >
+          <div className="pl-10 pr-20">-</div>
+          {t('is')}
+        </div>
+      </div>
+      <div>{t('description')}</div>
+    </div>
+  )
 }
