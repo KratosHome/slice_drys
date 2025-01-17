6dde13dcd566702267d0ca683a1ffa2330337b71
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-17 10:03:58 +0200

    fix (blog) blog title

--- src/app/(app)/[locale]/(client)/blog/page.tsx
+++ src/app/(app)/[locale]/(client)/blog/page.tsx
@@ -54,27 +54,7 @@ export default async function Blog({ params, searchParams }: Props) {
         </Breadcrumb>
       </div>
 
-      <div className="flex">
-        <div className="mb-10 mt-10 flex justify-between">
-          <div className="flex w-full flex-wrap justify-between">
-            <div className="flex w-[100%] min-w-[300px] sm:w-[34%]">
-              <div
-                className="w-[100%] text-[128px]"
-                style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
-              >
-                <BlogTitle variant="Blog" />
-              </div>
-            </div>
-
-            <div className="flex w-[100%] items-center sm:w-[65%]">
-              <div className="flex h-min w-full items-center justify-center bg-black p-5 text-left font-poppins text-xl text-white drop-shadow-[16px_-16px_0px_#A90909]">
-                <BlogTitle variant="title" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="my-4 border-t border-dashed border-black"></div>
-      </div>
+      <BlogTitle />
       <div className="mx-auto flex flex-col items-center">
         <PostList posts={data.post} />
         <Pagination>
--- src/components/client/ui/blog-title.tsx
+++ src/components/client/ui/blog-title.tsx
@@ -1,13 +1,30 @@
 import { useTranslations } from 'next-intl'
 
-// Визначаємо інтерфейс для параметра 'variant'
-interface BlogTitleProps {
-  variant: string
-}
-
-const BlogTitle = ({ variant }: BlogTitleProps) => {
+const BlogTitle = () => {
   const t = useTranslations('Blog')
-  return t(variant)
+  return (
+    <div className="flex">
+      <div className="mb-10 mt-10 flex justify-between">
+        <div className="flex w-full flex-wrap justify-between">
+          <div className="flex w-[100%] min-w-[300px] sm:w-[34%]">
+            <div
+              className="w-[100%] text-[128px]"
+              style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
+            >
+              {t('Blog')}
+            </div>
+          </div>
+
+          <div className="flex w-[100%] items-center sm:w-[65%]">
+            <div className="flex h-min w-full items-center justify-center bg-black p-5 text-left font-poppins text-xl text-white drop-shadow-[16px_-16px_0px_#A90909]">
+              {t('title')}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-4 border-t border-dashed border-black"></div>
+    </div>
+  )
 }
 
 export default BlogTitle
