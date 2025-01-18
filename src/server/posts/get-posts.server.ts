4fe31e1b4b0dd525d2e9d2ecea0cf9fd9f5e197b
olegtkach <olegontkach101@gmail.com>
2025-01-18 10:49:00 +0200

    refactor: getPosts

--- src/server/posts/get-posts.server.ts
+++ src/server/posts/get-posts.server.ts
@@ -3,61 +3,65 @@ import { connectToDb } from '@/server/connectToDb'
 import { Post } from '@/server/posts/postSchema'
 
 type GetPostsOptions = {
-  locale: string
+  locale: ILocale
   slug?: string
   page?: number
   limit?: number
+  isVisited?: boolean
 }
 
-export async function getPosts({ locale, slug, page, limit }: GetPostsOptions) {
+const getSelectedFields = (locale: ILocale) => ({
+  [`title.${locale}`]: 1,
+  [`content.${locale}`]: 1,
+  [`author.${locale}`]: 1,
+  [`metaDescription.${locale}`]: 1,
+  [`keywords.${locale}`]: 1,
+  img: 1,
+  slug: 1,
+  visited: 1,
+  updatedAt: 1,
+})
+
+const formatPost = (post: IPostLocal, locale: ILocale) => ({
+  _id: post._id?.toString(),
+  slug: post.slug,
+  img: post.img,
+  title: post.title[locale],
+  content: post.content[locale],
+  author: post.author[locale],
+  metaDescription: post.metaDescription[locale],
+  keywords: post.keywords[locale],
+  visited: post.visited,
+  updatedAt: post.updatedAt,
+})
+
+export async function getPosts({
+  locale,
+  slug,
+  page,
+  limit,
+  isVisited,
+}: GetPostsOptions) {
   try {
     await connectToDb()
 
     if (slug) {
-      const postQuery = Post.findOneAndUpdate(
-        { slug: slug },
-        { $inc: { visited: 1 } },
-        {
-          new: true,
-          select: {
-            [`title.${locale}`]: 1,
-            [`content.${locale}`]: 1,
-            img: 1,
-            [`author.${locale}`]: 1,
-            slug: 1,
-            [`metaDescription.${locale}`]: 1,
-            [`keywords.${locale}`]: 1,
-            visited: 1,
-            updatedAt: 1,
-          },
-        },
-      ).lean()
+      const post: IPostLocal | null = isVisited
+        ? ((await Post.findOneAndUpdate(
+            { slug },
+            { $inc: { visited: 1 } },
+            { new: true, select: getSelectedFields(locale) },
+          ).lean()) as IPostLocal | null)
+        : ((await Post.findOne(
+            { slug },
+            getSelectedFields(locale),
+          ).lean()) as IPostLocal | null)
 
-      const post = (await postQuery) as Any
-      if (!post) {
-        throw new Error('Пост не знайдено')
-      }
-
-      const formattedPost: IPost[] = [
-        {
-          _id: (post._id as string).toString(),
-          slug: post.slug,
-          img: post.img,
-          title: post.title[locale],
-          content: post.content[locale],
-          author: post.author[locale],
-          metaDescription: post.metaDescription[locale],
-          keywords: post.keywords[locale],
-          visited: post.visited,
-          updatedAt: post.updatedAt.$date
-            ? post.updatedAt.$date
-            : post.updatedAt,
-        },
-      ]
+      if (!post) return { success: false, post: [], message: 'Post not found' }
 
       return {
         success: true,
-        post: formattedPost,
+        post: [formatPost(post, locale)],
         postAll: [],
         totalPosts: 0,
         message: 'Products retrieved',
@@ -66,55 +70,20 @@ export async function getPosts({ locale, slug, page, limit }: GetPostsOptions) {
 
     const pagination = page && limit ? { skip: (page - 1) * limit, limit } : {}
 
-    const postQuery = Post.find()
-      .sort({ createdAt: -1 })
-      .select({
-        [`title.${locale}`]: 1,
-        [`content.${locale}`]: 1,
-        img: 1,
-        [`author.${locale}`]: 1,
-        slug: 1,
-        [`metaDescription.${locale}`]: 1,
-        [`keywords.${locale}`]: 1,
-        visited: 1,
-        updatedAt: 1,
-      })
-      .lean()
-
-    if (pagination.skip !== undefined && pagination.limit !== undefined) {
-      postQuery.skip(pagination.skip).limit(pagination.limit)
-    }
-
-    const totalPostsCount = await Post.countDocuments()
-
-    const post = await postQuery
-
-    const formattedPost: IPost[] = post.map((post) => ({
-      ...post,
-      _id: (post._id as string).toString(),
-      slug: post.slug,
-      img: post.img,
-      title: post.title[locale],
-      content: post.content[locale],
-      author: post.author[locale],
-      metaDescription: post.metaDescription[locale],
-      keywords: post.keywords[locale],
-      updatedAt: post.updatedAt.$date ? post.updatedAt.$date : post.updatedAt,
-    }))
-
-    const allPost: IPostLocal[] = (await Post.find()
-      .sort({ createdAt: -1 })
-      .lean()) as unknown as IPostLocal[]
-
-    const formattedAllPost = allPost.map((post) => ({
-      ...post,
-      _id: (post._id as string).toString(),
-    }))
+    const [posts, totalPostsCount, allPosts] = await Promise.all([
+      Post.find({}, getSelectedFields(locale))
+        .sort({ createdAt: -1 })
+        .skip(pagination.skip ?? 0)
+        .limit(pagination.limit ?? 0)
+        .lean<IPostLocal[]>(),
+      Post.countDocuments(),
+      Post.find().sort({ createdAt: -1 }).lean<IPostLocal[]>(),
+    ])
 
     return {
       success: true,
-      post: formattedPost,
-      postAll: formattedAllPost,
+      post: posts.map((p: IPostLocal) => formatPost(p, locale)),
+      postAll: allPosts.map((p) => ({ ...p, _id: p._id?.toString() })),
       totalPosts: totalPostsCount,
       message: 'Products retrieved',
     }
@@ -124,7 +93,7 @@ export async function getPosts({ locale, slug, page, limit }: GetPostsOptions) {
       post: [],
       postAll: [],
       totalPosts: 0,
-      message: "Can't retrieve posts" + { error },
+      message: `Can't retrieve posts ${error}`,
     }
   }
 }
