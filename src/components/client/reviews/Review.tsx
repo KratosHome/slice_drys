7ab50859695ccd3583d10b18c490782aab060fa9
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-20 11:52:15 +0200

    fix (about) review variant grey-white+

--- src/components/client/reviews/Review.tsx
+++ src/components/client/reviews/Review.tsx
@@ -48,7 +48,7 @@ export default function Reviews({ text, author, variant }: ReviewsProps) {
     ) : variant === 'grey-white' ? (
       <div className="mx-5 mx-auto my-10 max-w-[900px] pr-20">
         <div className="w-full bg-[#E4E4E4] p-5">
-          <div className="w-full bg-white p-5">
+          <div className="w-full bg-white p-2">
             <div
               className="h-0 -translate-x-16 -translate-y-16 text-[96px]"
               style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
@@ -56,7 +56,7 @@ export default function Reviews({ text, author, variant }: ReviewsProps) {
               “
             </div>
 
-            <div className="px-5">
+            <div className="px-2">
               <div className="pb-2 font-bold">{author}</div>
 
               <div>{text}</div>
