5ca2226eec535d26ff84d0b89940b8c0c426a2ae
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-20 12:24:11 +0200

    fix (about) review data

--- src/components/client/reviews/Reviews.tsx
+++ src/components/client/reviews/Reviews.tsx
@@ -2,12 +2,40 @@ import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Review from './Review'
 
+const reviewsData = [
+  {
+    author: 'Валера Валерʼян',
+    text: 'Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!',
+  },
+  {
+    author: 'Валера Валерʼян',
+    text: 'Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!',
+  },
+  {
+    author: 'Валера Валерʼян',
+    text: 'Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!',
+  },
+  {
+    author: 'Валера Валерʼян',
+    text: 'Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!',
+  },
+  {
+    author: 'Валера Валерʼян',
+    text: 'Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!',
+  },
+  {
+    author: 'Валера Валерʼян',
+    text: 'Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!',
+  },
+]
+const variants = ['grey', 'black', 'grey-white']
+
 export default function Reviews() {
   const t = useTranslations('Reviews')
 
   return (
-    <div>
-      <div className="flex flex-col items-center">
+    <div className="mt-40">
+      <div className="mb-20 flex flex-col items-center">
         <div
           style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
           className="inline-block pr-60 text-[96px]"
@@ -32,21 +60,16 @@ export default function Reviews() {
           </div>
         </div>
       </div>
-      <Review
-        author="Валера Валерʼян"
-        variant="grey"
-        text="Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!"
-      />
-      <Review
-        author="Валера Валерʼян"
-        variant="black"
-        text="Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!"
-      />
-      <Review
-        author="Валера Валерʼян"
-        variant="grey-white"
-        text="Сушені чіпси з м’яса – це просто фантастика! Легкий перекус для будь-якого дня. Залишився в захваті!"
-      />
+      <div>
+        {reviewsData.map((review, index) => (
+          <Review
+            key={index}
+            author={review.author}
+            text={review.text}
+            variant={variants[index % variants.length]}
+          />
+        ))}
+      </div>
     </div>
   )
 }
