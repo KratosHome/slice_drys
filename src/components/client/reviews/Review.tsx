840cb574c9e270ab90c314e65863b11b07b4835f
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-20 11:13:36 +0200

    feat (about) review variant black

--- src/components/client/reviews/Review.tsx
+++ src/components/client/reviews/Review.tsx
@@ -8,8 +8,8 @@ export default function Reviews({ text, author, variant }: ReviewsProps) {
   return (
     // Перевіряємо, чи варіант "grey", і рендеримо div лише в цьому випадку
     variant === 'grey' ? (
-      <div className="mx-5">
-        <div className="mx-auto w-full max-w-[800px] border border-black bg-[#E4E4E4]">
+      <div className="mx-5 mx-auto my-10 max-w-[900px] pr-16">
+        <div className="w-full border border-black bg-[#E4E4E4]">
           <div
             className="h-0 -translate-x-7 -translate-y-5 text-[96px]"
             style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
@@ -24,6 +24,21 @@ export default function Reviews({ text, author, variant }: ReviewsProps) {
           </div>
           <div
             className="h-0 -translate-y-20 translate-x-7 text-end text-[96px]"
+            style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
+          >
+            “
+          </div>
+        </div>
+      </div>
+    ) : variant === 'black' ? (
+      <div className="mx-5 mx-auto my-10 max-w-[900px]">
+        <div className="w-full border border-black bg-black text-white">
+          <div className="px-10 py-5 pr-40">
+            <div className="pb-2 font-bold">{author}</div>
+            <div>{text}</div>
+          </div>
+          <div
+            className="h-0 -translate-x-5 -translate-y-40 text-end text-[200px]"
             style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
           >
             “
