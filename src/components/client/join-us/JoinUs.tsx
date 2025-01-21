1345c95c55ed90766ec3601bfa202bfa5d0bc1d3
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-21 18:08:34 +0200

    fix (about) join  responsive

--- src/components/client/join-us/JoinUs.tsx
+++ src/components/client/join-us/JoinUs.tsx
@@ -8,7 +8,7 @@ export default function Reviews() {
     <div className="mt-60 flex flex-col justify-center bg-black py-7">
       <div className="mx-auto flex">
         <div className="align-start flex max-w-[1280] flex-col-reverse justify-end lg:flex-row">
-          <div className="flex h-[50%] -rotate-[17deg] flex-col lg:h-full lg:w-[40%]">
+          <div className="-mt-20 flex h-[50%] -rotate-[17deg] flex-col lg:mt-0 lg:h-full lg:w-[40%]">
             <div
               style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
               className="hidden translate-y-8 rotate-[30deg] text-center text-[48px] text-red lg:block lg:-translate-x-10 lg:rotate-0"
@@ -39,20 +39,20 @@ export default function Reviews() {
             <div className="max-w-[75%] text-start text-[24px] text-white lg:max-w-[600px]">
               {t('be first')}
             </div>
-            <div className="ml-auto mt-8 flex h-[20%] w-[50%] -translate-x-[150%] translate-y-2 -rotate-12 flex-row-reverse bg-red lg:translate-x-12 lg:flex-row">
+            <div className="ml-auto mt-8 flex w-[50%] -translate-x-[130%] translate-y-[60%] -rotate-12 justify-end bg-red md:h-32 lg:h-[20%] lg:translate-x-12 lg:translate-y-2 lg:justify-start">
               <Image
                 src={'/icons/instagram-white.svg'}
                 alt="instagram icon"
-                width={60}
-                height={60}
-                className="my-auto ml-5 h-[65%] cursor-pointer"
+                width={160}
+                height={160}
+                className="my-auto ml-5 h-[65%] w-[40%] cursor-pointer pl-10 md:mr-4 md:w-20 md:pl-0 lg:mr-0 lg:w-16"
               />
               <Image
                 src={'/icons/facebook-white.svg'}
                 alt="facebook icon"
-                width={60}
-                height={60}
-                className="my-auto ml-5 h-[65%] cursor-pointer"
+                width={160}
+                height={160}
+                className="mx-4 my-auto h-[65%] w-[40%] cursor-pointer md:mx-8 md:ml-5 md:w-20 lg:w-16"
               />
             </div>
           </div>
