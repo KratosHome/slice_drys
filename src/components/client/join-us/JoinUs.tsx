2873238bfd89751fc1efdcc763cef5ce088f3158
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-21 17:38:53 +0200

    fix (about) bite  responsive

--- src/components/client/join-us/JoinUs.tsx
+++ src/components/client/join-us/JoinUs.tsx
@@ -7,33 +7,39 @@ export default function Reviews() {
   return (
     <div className="mt-60 flex flex-col justify-center bg-black py-7">
       <div className="mx-auto flex">
-        <div className="align-center flex max-w-[1280]">
-          <div className="flex w-[40%] -rotate-[17deg] flex-col">
+        <div className="align-start flex max-w-[1280] flex-col-reverse justify-end lg:flex-row">
+          <div className="flex h-[50%] -rotate-[17deg] flex-col lg:h-full lg:w-[40%]">
             <div
               style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
-              className="-translate-x-10 translate-y-3 text-center text-[48px] text-red"
+              className="hidden translate-y-8 rotate-[30deg] text-center text-[48px] text-red lg:block lg:-translate-x-10 lg:rotate-0"
+            >
+              {t('bite')}
+            </div>
+            <div
+              style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
+              className="ml-auto translate-x-8 text-[48px] text-red lg:hidden"
             >
               {t('bite')}
             </div>
             <Image
-              className="w-full"
+              className="-mt-32 ml-auto mr-20 w-[50%] lg:mr-0 lg:mt-0 lg:w-full"
               src={'/images/iPhone-15-Pro.png'}
               alt="iPhone image"
               width={520}
               height={440}
             />
           </div>
-          <div className="ml-10 flex w-[60%] flex-col justify-center">
+          <div className="ml-10 flex flex-col justify-center lg:w-[60%]">
             <div
               style={{ fontFamily: 'var(--font-rubik-doodle-shadow)' }}
-              className="text-start text-[80px] leading-tight text-white"
+              className="text-start text-[75px] leading-tight text-white"
             >
               {t('join us')}
             </div>
-            <div className="max-w-[600px] text-start text-[24px] text-white">
+            <div className="max-w-[75%] text-start text-[24px] text-white lg:max-w-[600px]">
               {t('be first')}
             </div>
-            <div className="ml-auto mt-8 flex h-[20%] w-[50%] translate-x-12 translate-y-2 -rotate-12 bg-red">
+            <div className="ml-auto mt-8 flex h-[20%] w-[50%] -translate-x-[150%] translate-y-2 -rotate-12 flex-row-reverse bg-red lg:translate-x-12 lg:flex-row">
               <Image
                 src={'/icons/instagram-white.svg'}
                 alt="instagram icon"
