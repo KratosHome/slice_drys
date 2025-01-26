e0e0b83df9220fafc7e4a8a56b42095c8661a6a4
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-27 00:11:58 +0200

    feat (np) update np data

--- src/server/delivery/update-nova-poshta.ts
+++ src/server/delivery/update-nova-poshta.ts
@@ -15,28 +15,95 @@ export async function createPost() {
       ],
     }
 
-    const novaPoshta = new NovaPoshta(NovaPoshtaData)
-    await novaPoshta.save()
-
-    const response = await fetch('https://api.novaposhta.ua/v2.0/json/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        apiKey: '',
-        modelName: 'Address',
-        calledMethod: 'getCities',
-      }),
-    })
-    const data = await response.json()
-    const cities = data.data.map(
-      (city: { Description: string; Ref: string }) => ({
+    async function fetchCities() {
+      const response = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          apiKey: '',
+          modelName: 'Address',
+          calledMethod: 'getCities',
+        }),
+      })
+      const data = await response.json()
+      return data.data.map((city: { Description: string; Ref: string }) => ({
         Description: city.Description,
         Ref: city.Ref,
-      }),
+      }))
+    }
+
+    const cities = await fetchCities()
+
+    async function fetchBrunchesByPage(i: number) {
+      const response = await fetch('https://api.novaposhta.ua/v2.0/json/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          apiKey: '',
+          modelName: 'Address',
+          calledMethod: 'getWarehouses',
+          methodProperties: {
+            Page: i,
+          },
+        }),
+      })
+      const data = await response.json()
+      return data
+    }
+
+    const branchesTotalCount = await fetchBrunchesByPage(1)
+    const totalPage = Math.ceil(branchesTotalCount.info.totalCount / 500)
+
+    let combinedBrunchData: {
+      Description: string
+      CityRef: string
+      CityName?: string
+    }[] = []
+
+    for (let i = 1; i <= totalPage; i++) {
+      //!!! totalPage
+      const brunch = await fetchBrunchesByPage(i)
+      const brunchData = brunch.data.map(
+        (brunch: { Description: string; CityRef: string }) => {
+          const city = cities.find(
+            (city: { Description: string; Ref: string }) =>
+              city.Ref === brunch.CityRef,
+          )
+
+          return {
+            Branch: brunch.Description,
+            City: city ? city.Description : 'Unknown',
+          }
+        },
+      )
+
+      combinedBrunchData = [...combinedBrunchData, ...brunchData]
+    }
+
+    const groupedData = combinedBrunchData.reduce(
+      (acc, item) => {
+        const existingCity = acc.find((data) => data.city === item.City)
+        if (existingCity) {
+          existingCity.branches.push(item.Branch)
+        } else {
+          acc.push({ city: item.City, branches: [item.Branch] })
+        }
+        return acc
+      },
+      [] as { city: string; branches: string[] }[],
     )
-    console.log(11111111111111, cities)
+
+    groupedData.forEach(async (NovaPoshtaData) => {
+      await NovaPoshta.updateOne(
+        { city: NovaPoshtaData.city }, // знайти документ за назвою міста
+        { $set: NovaPoshtaData }, // оновити поле branches
+        { upsert: true }, // створити новий документ, якщо не знайдено відповідного
+      )
+    })
 
     return { success: true, message: 'nova poshta data updated' }
   } catch (error) {
