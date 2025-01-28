2098ab7ec74c44ef0d3fc48c76b2b97b273862aa
dromse <dromasko4@gmail.com>
2025-01-28 16:14:59 +0200

    feat: get product by slug, transition from main slider

--- src/app/(app)/[locale]/(client)/products/[slug]/page.tsx
+++ src/app/(app)/[locale]/(client)/products/[slug]/page.tsx
@@ -1,13 +1,29 @@
 import { ProductInfo } from '@/components/client/product-page'
 import { Accordions } from '@/components/client/product-page/accordions'
 import { Breadcrumbs } from '@/components/client/product-page/breadcrumbs'
+import { getProductBySlug } from '@/server/products/get-product-by-slug.server'
 
-export default function Page() {
+export default async function ProductPage({ params }: IPage) {
+  const { slug, locale } = params
+
+  const productJSON = await getProductBySlug({
+    slug,
+    locale,
+  })
+
+  const res = JSON.parse(productJSON)
+
+  if (!res.success) {
+    return <>Product not found</>
+  }
+
+  const { product } = res
+
   return (
     <div className="container px-5 font-poppins lg:px-0">
       <Breadcrumbs />
-      <ProductInfo />
-      <Accordions />
+      <ProductInfo product={product} />
+      <Accordions nutritions={product.nutritionalValue} />
     </div>
   )
 }
