8f411358ecea45d2b5324ac9e4255d66fa082c61
dromse <dromasko4@gmail.com>
2025-01-28 16:14:59 +0200

    feat: get product by slug, transition from main slider

--- src/components/client/product-page/accordions.tsx
+++ src/components/client/product-page/accordions.tsx
@@ -4,26 +4,52 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/client/ui/accordion'
-import { accordions } from './consts'
+import { infoAccordions } from './consts'
 
-export const Accordions = () => (
-  <Accordion type="multiple" className="mx-auto grid max-w-[988px] gap-6">
-    {accordions.map((accordion) => (
-      <AccordionItem value={accordion.title} key={accordion.title}>
-        <AccordionTrigger>{accordion.title}</AccordionTrigger>
+export const Accordions = ({
+  nutritions,
+}: {
+  nutritions: INutritionalValue
+}) => {
+  const nutritionsAccordion = {
+    title: 'Поживна (харчова) цінність на 100 г продукту',
+    fields: [
+      { label: 'Білки', value: nutritions.proteins + ' г' },
+      { label: 'Жири', value: nutritions.fats + ' г' },
+      {
+        label: 'Вуглеводи',
+        value: nutritions.carbohydrates + ' г',
+      },
+      {
+        label: 'Енергетична цінність',
+        value: nutritions.energyValue + ' г',
+      },
+    ],
+  }
 
-        <AccordionContent>
-          {accordion.fields.map((field) => (
-            <section
-              key={field.label}
-              className="flex flex-wrap gap-5 sm:flex-nowrap"
-            >
-              <dt className="min-w-48 font-bold sm:min-w-60">{field.label}</dt>
-              <dd className="sm:min-w-40">{field.value}</dd>
-            </section>
-          ))}
-        </AccordionContent>
-      </AccordionItem>
-    ))}
-  </Accordion>
-)
+  const accordions = infoAccordions.toSpliced(1, 0, nutritionsAccordion)
+
+  return (
+    <Accordion type="multiple" className="mx-auto grid max-w-[988px] gap-6">
+      {accordions.map((accordion) => (
+        <AccordionItem value={accordion.title} key={accordion.title}>
+          <AccordionTrigger>{accordion.title}</AccordionTrigger>
+
+          <AccordionContent>
+            {accordion.fields.map((field) => (
+              <section
+                key={field.label}
+                className="flex flex-wrap gap-5 sm:flex-nowrap"
+              >
+                <dt className="min-w-48 font-bold sm:min-w-60">
+                  {field.label}
+                </dt>
+                <dd className="sm:min-w-40">{field.value}</dd>
+              </section>
+            ))}
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  )
+}
