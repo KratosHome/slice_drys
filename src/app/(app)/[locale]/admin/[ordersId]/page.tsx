6a599dfed874de4f4863de47f296ef759ce70d41
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2025-01-28 13:02:19 +0200

    feat (orders admin) get-orders

--- src/app/(app)/[locale]/admin/[ordersId]/page.tsx
+++ src/app/(app)/[locale]/admin/[ordersId]/page.tsx
@@ -3,57 +3,11 @@ import OrdersList from '@/components/admin/orders/orders-list'
 import { getOrders } from '@/server/orders/get-orders'
 
 export default async function ProductPage() {
-  const dataOrders: IOrder[] = [
-    {
-      id: '1',
-      status: 'new',
-      products: [
-        { id: '1', name: 'Product 1', count: 1, price: 100 },
-        { id: '2', name: 'Product 2', count: 2, price: 200 },
-      ],
-      total: 300,
-      user: {
-        id: '1',
-        name: 'Name',
-        surname: 'Surname',
-        phone: '380123456789',
-        email: 'example@example.com',
-      },
-      delivery: {
-        city: 'Kyiv',
-        department: 'Department 1',
-        phone: '380123456789',
-      },
-      payment: { method: 'cash' },
-      comment: 'Some comment',
-    },
-    {
-      id: '2',
-      status: 'awaitingPayment',
-      products: [{ id: '3', name: 'Product 3', count: 1, price: 150 }],
-      total: 150,
-      user: {
-        id: '2',
-        name: 'John',
-        surname: 'Doe',
-        phone: '380987654321',
-        email: 'john.doe@example.com',
-      },
-      delivery: {
-        city: 'Lviv',
-        department: 'Department 5',
-        phone: '380987654321',
-      },
-      payment: { method: 'card' },
-      comment: 'Urgent delivery',
-    },
-  ]
-  const { orders } = await getOrders(1, 1)
-  console.log(orders)
+  const { orders } = await getOrders()
 
   return (
     <div>
-      <OrdersList data={dataOrders} />
+      <OrdersList data={orders || []} />
     </div>
   )
 }
--- src/server/orders/get-orders.ts
+++ src/server/orders/get-orders.ts
@@ -1,21 +1,44 @@
 import { connectToDb } from '@/server/connectToDb'
 import { Order } from './orderSchema'
 
-export async function getOrders(page: number, limit: number) {
+export async function getOrders() {
   try {
     await connectToDb()
 
-    const skip = (page - 1) * limit
+    const orderDocs = await Order.find()
 
-    const orders = await Order.find().skip(skip).limit(limit)
+    const orders: IOrder[] = orderDocs.map((orderDoc) => ({
+      id: orderDoc._id.toString(),
+      status: orderDoc.status,
+      products: orderDoc.products.map((product: any) => ({
+        id: product.id,
+        name: product.name,
+        count: product.count,
+        price: product.price,
+      })),
+      total: orderDoc.total,
+      user: {
+        id: orderDoc.user.id,
+        name: orderDoc.user.name,
+        surname: orderDoc.user.surname,
+        phone: orderDoc.user.phone,
+        email: orderDoc.user.email,
+      },
+      delivery: {
+        city: orderDoc.delivery.city,
+        department: orderDoc.delivery.department,
+        phone: orderDoc.delivery.phone,
+      },
+      payment: {
+        method: orderDoc.payment.method,
+      },
+      comment: orderDoc.comment || '',
+    }))
 
-    // Отримуємо загальну кількість замовлень
     const totalOrders = await Order.countDocuments()
 
-    // Повертаємо об'єкт з замовленнями і загальною кількістю
     return { success: true, orders, totalOrders }
   } catch (error) {
-    // Обробка помилки
     return {
       success: false,
       message: `Cannot retrieve orders: ${error}`,
