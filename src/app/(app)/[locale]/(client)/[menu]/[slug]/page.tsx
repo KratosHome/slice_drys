c2f050ff5cd12adf8696764d00f76cd97ccf544d
olegtkach <olegontkach101@gmail.com>
2025-03-09 18:09:26 +0200

    fix(client): product build

--- src/app/(app)/[locale]/(client)/[menu]/[slug]/page.tsx
+++ src/app/(app)/[locale]/(client)/[menu]/[slug]/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = 'force-dynamic'
-export const revalidate = 86400
+export const revalidate = 0
 
 import { ProductInfo } from '@/components/client/product-page'
 import { Accordions } from '@/components/client/product-page/accordions'
