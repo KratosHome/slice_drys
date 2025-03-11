4be06d3b9f9389baa04ad334c08f01f4b3ddbcdf
olegtkach <olegontkach101@gmail.com>
2025-03-11 19:38:14 +0200

    test(client): speed

--- src/app/(app)/[locale]/(client)/page.tsx
+++ src/app/(app)/[locale]/(client)/page.tsx
@@ -4,22 +4,12 @@ import { getTranslations } from 'next-intl/server'
 import { Hero } from '@/components/client/main/hero'
 import ProductSlider from '@/components/client/product-slider/product-slider'
 import { detectDevice } from '@/utils/deviceDetection'
-import Faq from '@/components/client/main/faq/faq'
-import InstaFeed from '@/components/client/main/instaFeed/InstaFeed'
-import Help from '@/components/client/main/help/help'
-import Reviews from '@/components/client/main/reviews/reviews'
-import Partners from '@/components/client/main/partners'
-import BlogSection from '@/components/client/main/blog/blog'
-import ToTheTop from '@/components/client/ui/to-the-top'
-
-import { partnersData } from '@/data/main/partners'
 import { faqData } from '@/data/main/faq'
 import type { Metadata } from 'next'
 import { mainMetaData } from '@/data/meta-data/main'
 import { locales } from '@/data/locales'
 import MainJsonLd from '@/components/client/json-ld/main-json-ld'
 import { reviewsData } from '@/data/main/reviews'
-import { instaData } from '@/data/main/insta-data'
 import { fetchTags } from '@/data/fetch-tags'
 
 export async function generateMetadata({
@@ -46,7 +36,38 @@ export default async function Home(props: {
   const userAgent: string = (await headers()).get('user-agent') || ''
   const device: IDevice = detectDevice(userAgent)
 
-  const [productsData, blogData, categoriesData, helpData] = await Promise.all([
+  const [productsData, categoriesData] = await Promise.all([
+    fetch(`${url}/api/products/get-products-slider-main?locale=${locale}`, {
+      cache: 'force-cache',
+      next: { tags: [`${fetchTags.products}`] },
+    }).then((res) => res.json()),
+
+    await fetch(`${url}/api/categories`, {
+      cache: 'force-cache',
+      next: { tags: [`${fetchTags.menu}`] },
+    }).then((res) => res.json()),
+  ])
+
+  return (
+    <>
+      <MainJsonLd
+        products={productsData.products}
+        faq={faqData[locale]}
+        reviews={reviewsData}
+      />
+      <Hero device={device} productLinks={categoriesData.data} />
+      <ProductSlider
+        products={productsData.products}
+        title={t('title')}
+        message={t('message')}
+      />
+    </>
+  )
+}
+
+/*
+
+ const [productsData, blogData, categoriesData, helpData] = await Promise.all([
     fetch(`${url}/api/products/get-products-slider-main?locale=${locale}`, {
       cache: 'force-cache',
       next: { tags: [`${fetchTags.products}`] },
@@ -68,19 +89,7 @@ export default async function Home(props: {
     }).then((res) => res.json()),
   ])
 
-  return (
-    <>
-      <MainJsonLd
-        products={productsData.products}
-        faq={faqData[locale]}
-        reviews={reviewsData}
-      />
-      <Hero device={device} productLinks={categoriesData.data} />
-      <ProductSlider
-        products={productsData.products}
-        title={t('title')}
-        message={t('message')}
-      />
+
       <Help data={helpData.data} />
       <Faq data={faqData[locale]} />
       <Partners data={partnersData[locale]} />
@@ -88,6 +97,4 @@ export default async function Home(props: {
       <Reviews reviews={reviewsData} />
       <InstaFeed data={instaData[locale]} />
       <ToTheTop />
-    </>
-  )
-}
+ */
