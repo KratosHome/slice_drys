4aacc25ed12af3387cff8a66222f94e0e91f9041
olegtkach <olegontkach101@gmail.com>
2025-03-11 19:20:33 +0200

    test(client): speed

--- src/app/(app)/[locale]/(client)/page.tsx
+++ src/app/(app)/[locale]/(client)/page.tsx
@@ -2,24 +2,25 @@ import { headers } from 'next/headers'
 import { getTranslations } from 'next-intl/server'
 
 import { Hero } from '@/components/client/main/hero'
+import ProductSlider from '@/components/client/product-slider/product-slider'
 import { detectDevice } from '@/utils/deviceDetection'
+import Faq from '@/components/client/main/faq/faq'
+import InstaFeed from '@/components/client/main/instaFeed/InstaFeed'
+import Help from '@/components/client/main/help/help'
+import Reviews from '@/components/client/main/reviews/reviews'
+import Partners from '@/components/client/main/partners'
+import BlogSection from '@/components/client/main/blog/blog'
+import ToTheTop from '@/components/client/ui/to-the-top'
 
+import { partnersData } from '@/data/main/partners'
 import { faqData } from '@/data/main/faq'
 import type { Metadata } from 'next'
 import { mainMetaData } from '@/data/meta-data/main'
 import { locales } from '@/data/locales'
 import MainJsonLd from '@/components/client/json-ld/main-json-ld'
 import { reviewsData } from '@/data/main/reviews'
+import { instaData } from '@/data/main/insta-data'
 import { fetchTags } from '@/data/fetch-tags'
-import dynamic from 'next/dynamic'
-import Help from '@/components/client/main/help/help'
-
-const ProductSlider = dynamic(
-  () => import('@/components/client/product-slider/product-slider'),
-  {
-    loading: () => <></>,
-  },
-)
 
 export async function generateMetadata({
   params,
@@ -45,10 +46,15 @@ export default async function Home(props: {
   const userAgent: string = (await headers()).get('user-agent') || ''
   const device: IDevice = detectDevice(userAgent)
 
-  const [productsData, categoriesData, helpData] = await Promise.all([
+  const [productsData, blogData, categoriesData, helpData] = await Promise.all([
     fetch(`${url}/api/products/get-products-slider-main?locale=${locale}`, {
       cache: 'force-cache',
       next: { tags: [`${fetchTags.products}`] },
+    }).then((res) => res.json()),
+
+    fetch(`${url}/api/posts?locale=${locale}&page=1&limit=5`, {
+      cache: 'force-cache',
+      next: { tags: [`${fetchTags.posts}`] },
     }).then((res) => res.json()),
 
     await fetch(`${url}/api/categories`, {
@@ -61,9 +67,6 @@ export default async function Home(props: {
       next: { tags: [`${fetchTags.helpMain}`] },
     }).then((res) => res.json()),
   ])
-
-  // eslint-disable-next-line no-console
-  console.log('productsData', productsData)
 
   return (
     <>
@@ -79,38 +82,12 @@ export default async function Home(props: {
         message={t('message')}
       />
       <Help data={helpData.data} />
-    </>
-  )
-}
-
-/*
-
-  const [productsData, blogData, categoriesData, helpData] = await Promise.all([
-    fetch(`${url}/api/products/get-products-slider-main?locale=${locale}`, {
-      cache: 'force-cache',
-      next: { tags: [`${fetchTags.products}`] },
-    }).then((res) => res.json()),
-
-    fetch(`${url}/api/posts?locale=${locale}&page=1&limit=5`, {
-      cache: 'force-cache',
-      next: { tags: [`${fetchTags.posts}`] },
-    }).then((res) => res.json()),
-
-
-    await fetch(`${url}/api/block/help?locale=${locale}`, {
-      cache: 'force-cache',
-      next: { tags: [`${fetchTags.helpMain}`] },
-    }).then((res) => res.json()),
-  ])
-
-
-
-
-      <Help data={helpData.data} />
       <Faq data={faqData[locale]} />
       <Partners data={partnersData[locale]} />
       <BlogSection data={blogData.postsLocalized} />
       <Reviews reviews={reviewsData} />
       <InstaFeed data={instaData[locale]} />
       <ToTheTop />
- */
+    </>
+  )
+}
