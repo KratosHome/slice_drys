2edf4591d9ff8bf016daca9282bb80b5a78ad7b0
olegtkach <olegontkach101@gmail.com>
2025-03-11 18:28:50 +0200

    test(client): speed

--- src/app/(app)/[locale]/(client)/page.tsx
+++ src/app/(app)/[locale]/(client)/page.tsx
@@ -1,6 +1,10 @@
+import { faqData } from '@/data/main/faq'
 import type { Metadata } from 'next'
 import { mainMetaData } from '@/data/meta-data/main'
 import { locales } from '@/data/locales'
+import MainJsonLd from '@/components/client/json-ld/main-json-ld'
+import { reviewsData } from '@/data/main/reviews'
+import { fetchTags } from '@/data/fetch-tags'
 
 export async function generateMetadata({
   params,
@@ -16,16 +20,32 @@ export async function generateStaticParams() {
   return locales.map((locale) => ({ locale }))
 }
 
-export default async function Home() {
-  return <>nest</>
+export default async function Home(props: {
+  params: Params
+  searchParams: ISearchParams
+}) {
+  const url = process.env.NEXT_URL
+  const { locale } = await props.params
+
+  const [productsData] = await Promise.all([
+    fetch(`${url}/api/products/get-products-slider-main?locale=${locale}`, {
+      cache: 'force-cache',
+      next: { tags: [`${fetchTags.products}`] },
+    }).then((res) => res.json()),
+  ])
+
+  return (
+    <>
+      <MainJsonLd
+        products={productsData.products}
+        faq={faqData[locale]}
+        reviews={reviewsData}
+      />
+    </>
+  )
 }
 
 /*
-  const url = process.env.NEXT_URL
-  const { locale } = await props.params
-  const t = await getTranslations('main.products-slider')
-  const userAgent: string = (await headers()).get('user-agent') || ''
-  const device: IDevice = detectDevice(userAgent)
 
   const [productsData, blogData, categoriesData, helpData] = await Promise.all([
     fetch(`${url}/api/products/get-products-slider-main?locale=${locale}`, {
@@ -51,11 +71,6 @@ export default async function Home() {
 
 
 
-      <MainJsonLd
-        products={productsData.products}
-        faq={faqData[locale]}
-        reviews={reviewsData}
-      />
       <Hero device={device} productLinks={categoriesData.data} />
       <ProductSlider
         products={productsData.products}
