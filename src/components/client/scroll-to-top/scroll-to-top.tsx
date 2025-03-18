5ed8e8129ccba94582657d0e3c23e85f0935a561
olegtkach <olegontkach101@gmail.com>
2025-03-18 21:58:57 +0200

    fix(client): scroll to top

--- src/components/client/scroll-to-top/scroll-to-top.tsx
+++ src/components/client/scroll-to-top/scroll-to-top.tsx
@@ -1,10 +1,13 @@
 'use client'
 import React, { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 
 const ScrollToTop: React.FC = () => {
+  const pathname = usePathname()
+
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [])
+  }, [pathname])
 
   return null
 }
