4d8aabeef7dea98ed9dffc93d64667843f207535
olegtkach <olegontkach101@gmail.com>
2025-03-21 15:15:14 +0200

    fix(client): login

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
 import { NextRequest } from 'next/server'
 
-const isTesting = process.env.NEXT_STATUS === 'test'
 const USERNAME = '1'
 const PASSWORD = '1'
 
@@ -16,8 +15,16 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const adminRegex = /^\/((en|uk)\/)?admin(\/|$)/
 
-  if (isTesting) {
-    const expectedAuth = `Basic ${Buffer.from(`${USERNAME}:${PASSWORD}`).toString('base64')}`
+  if (adminRegex.test(pathname)) {
+    const expectedAdminAuth = `Basic ${Buffer.from(`${ADMIN_USERNAME}:${ADMIN_PASSWORD}`).toString('base64')}` // Використано правильний синтаксис шаблонних рядків
+    if (auth !== expectedAdminAuth) {
+      return new Response('Admin authentication required', {
+        status: 401,
+        headers: { 'WWW-Authenticate': 'Basic realm="Admin Protected"' },
+      })
+    }
+  } else {
+    const expectedAuth = `Basic ${Buffer.from(`${USERNAME}:${PASSWORD}`).toString('base64')}` // Використано правильний синтаксис шаблонних рядків
     if (auth !== expectedAuth) {
       return new Response('Authentication required', {
         status: 401,
@@ -26,17 +33,6 @@ export async function middleware(request: NextRequest) {
     }
   }
 
-  if (adminRegex.test(pathname)) {
-    const expectedAdminAuth = `Basic ${Buffer.from(`${ADMIN_USERNAME}:${ADMIN_PASSWORD}`).toString('base64')}`
-    if (auth !== expectedAdminAuth) {
-      return new Response('Admin authentication required', {
-        status: 401,
-        headers: { 'WWW-Authenticate': 'Basic realm="Admin Protected"' },
-      })
-    }
-  }
-
-  // Лише після успішної авторизації передаємо керування до next-intl middleware
   return intlMiddleware(request)
 }
 
