e561480fa1fa322011e13526dabb39ae0ad5dad9
olegtkach <olegontkach101@gmail.com>
2025-03-28 18:35:49 +0200

    fix(client): url

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,5 +29,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/admin/:path*', '/(en|uk)/admin/:path*'],
+  matcher: ['/', '/(en|uk)/:path*'],
 }
