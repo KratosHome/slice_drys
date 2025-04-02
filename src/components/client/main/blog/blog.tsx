33f4cc838e20034d25325f4a9ada0b4634665411
olegtkach <olegontkach101@gmail.com>
2025-04-02 09:56:53 +0300

    fix: style main

--- src/components/client/main/blog/blog.tsx
+++ src/components/client/main/blog/blog.tsx
@@ -11,6 +11,7 @@ import { formatDate } from '@/utils/format-date'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { UnderlinedLink } from '@/components/ui/underlined-link'
 import { useIsMobile } from '@/hooks/use-mobile'
+import '../../styles/slider.css'
 import '@splidejs/react-splide/css'
 import './blog.css'
 
@@ -76,9 +77,9 @@ const BlogSection: FC<BlogSectionProps> = ({ data }) => {
       aria-labelledby="more about us"
       className="section about-us bg-black text-white"
     >
-      <div className="mx-auto flex max-w-[1440px] flex-col items-center px-[20px] py-[32px] md:block md:px-[clamp(20px,calc(20px+80*(100vw-768px)/672),100px)] lg:py-[52px]">
+      <div className="about__wrapper">
         <div className="mb-6 contents items-center justify-between md:flex md:gap-[40px]">
-          <h2 className="title-section text-center normal-case! md:text-start">
+          <h2 className="title-section text-center !normal-case md:text-start">
             {t('title')}
           </h2>
           <UnderlinedLink
@@ -107,15 +108,14 @@ const BlogSection: FC<BlogSectionProps> = ({ data }) => {
                 arrow: 'splide__arrow custom__arrow',
                 prev: 'splide__arrow--prev custom__arrow-prev',
                 next: 'splide__arrow--next custom__arrow-next',
-                pagination:
-                  'bottom-[-80px] gap-[3px] md:bottom-[-77px] xl:bottom-[-79px]',
+                pagination: 'splide__pagination custom__pagination',
                 page: 'splide__pagination__page custom__pagination-page',
               },
             }}
           >
             {data.map((post) => (
               <SplideSlide key={post._id}>
-                <div className="relative h-full rounded-xl bg-gray-900 transition-all duration-300 ease-in-out will-change-transform active:-translate-y-1 active:shadow-[0px_4px_10px_3px_rgba(255,255,255,0.15)] lg:hover:-translate-y-1 lg:hover:shadow-[0px_4px_10px_3px_rgba(255,255,255,0.15)]">
+                <div className="about__card">
                   <AspectRatio ratio={355 / 285} className="h-full">
                     <Image
                       src={post.img}
@@ -130,7 +130,7 @@ const BlogSection: FC<BlogSectionProps> = ({ data }) => {
                     </p>
                     <Link
                       href={`/${locale}/blog/${post.slug}`}
-                      className="text-[clamp(16px,calc(16px+4*(100vw-768px)/672),20px)] font-medium text-balance before:absolute before:inset-0 before:cursor-pointer"
+                      className="card__link"
                     >
                       {post.title}
                     </Link>
